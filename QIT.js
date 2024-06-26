28b10ef49a4e1e3f705fc74d7a6c554943e30cf8
Gabby Strevay <gstrevay@Gabbys-MacBook-Air.local>
2024-06-25 17:11:41 -0700

    Initial commit

---
+++ QIT.js
@@ -0,0 +1,8 @@
+// scrolls down Y 1000px
+function scrollFunction1000() {
+    window.scroll(0, window.scrollY + 1000);
+}
+
+function scrollFunction() {
+    window.scroll(0, window.scrollY +1500);
+}
