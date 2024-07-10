f243278249252f0c48175b39dd77e75fe22ff4c5
Gabby Strevay <gstrevay@Gabbys-MacBook-Air.local>
2024-07-09 19:35:33 -0700

    further updates

--- QIT.js
+++ QIT.js
@@ -3,6 +3,6 @@ function scrollFunction1000() {
     window.scroll(0, window.scrollY + 1000);
 }
 
-function scrollFunction() {
-    window.scroll(0, window.scrollY +1500);
+function scrollFunction900() {
+    window.scroll(0, window.scrollY + 900);
 }
